e6d247fb9c995480d0a3dac4b5b60529919e6718
aot <ahmet@netenets.com>
2016-10-04 15:24:18 +0300

    add gemoji

--- console.icon.js
+++ console.icon.js
@@ -6,67 +6,29 @@
 */
 
 (function thing() {
-  // window.console or the normal console.
-  var con = console;
-  if (typeof(window) !== 'undefined') {
-    con = window.console;
-  }
 
-  con.icon = function() {
+    var gemoji = require('./node_modules/gemoji');
 
-    var iconNames = [
-        ":D", 
-        ":)", 
-        ":*", 
-        "B)", 
-        ":|", 
-        ":o", 
-        ":p",
-        ":/",
-        ":(",
-        ":@",
-        "poop",
-        "<3",
-        "zzz",
-        "no-entry",
-        "check",
-        "crossmark",
-        "questionmark",
-        "copyright",
-        "registered",
-        "trademark",
-        "blacksquare",
-        "whitesquare",
-        "blackcircle",
-        "whitecircle",
-        "bomb",
-        "bulb",
-        "envelope",
-        "pushpin",
-        "key",
-        "lock",
-        "cloud",
-        "droplet",
-        "snowflake",
-        "highvoltage",
-        "construction",
-        "videogame",
-        "beer",
-        "shrug",
-        "birthday"];
-        
-    var iconTable = ["😀","😊","😘","😎","😐","😯","😜","😕","😞","😡","💩","❤️","💤","⛔","✔","❌","❔","©","®","™","⬛","⬜","⚫","⚪","💣","💡","✉","📌","🔑","🔒","☁","💧","❄","⚡","🚧","🎮","🍺","🤷","🎂"];
-    var args = Array.prototype.slice.call(arguments);
-    var index = iconNames.indexOf(args[0]);
-    if(index != -1){
-        if(args[1] != undefined){
-            console.log(iconTable[index]+" "+ args[1]);
-        } else {
-            console.log(iconTable[index]);
-        }
-    } else {
-        console.error("❕ undefined icon");
+    // window.console or the normal console.
+    var con = console;
+    if (typeof (window) !== 'undefined') {
+        con = window.console;
     }
+    con.icon = function () {
 
-  }
+        var args = Array.prototype.slice.call(arguments);
+        //var index = iconNames.indexOf(args[0]);
+        /*if(index != -1){
+    
+        } else {
+            console.error("❕ undefined icon");
+        }*/
+        if (args[1] != undefined) {
+            console.log(gemoji.name[args[0]].emoji + " " + args[1]);
+        } else {
+            console.log(gemoji.name[args[0]].emoji);
+        }
+
+
+    }
 })();
